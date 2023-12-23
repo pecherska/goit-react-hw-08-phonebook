042369fe8eda5fe556b265bea9a7f87913d29666
Nina Pecherska <npecherska@gmail.com>
2023-12-23 20:08:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store, persistor } from 'components/store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
